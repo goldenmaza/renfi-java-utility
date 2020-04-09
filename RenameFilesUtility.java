440d37d8b801e754f93038f7d18cf597b3eaad20
Mats Richard Hellstrand <goldenmaza@outlook.com>
2020-04-09 18:09:25 +0200

    [Renfi] Dynamic commands and extensions have been implemented

--- RenameFilesUtility.java
+++ RenameFilesUtility.java
@@ -6,19 +6,32 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class RenameFilesUtility {
+	private static final int DIR_INDEX = 0;
+	private static final int COMMAND_INDEX = 1;
+	private static final int EXTENSION_INDEX = 2;
+	private static final String BABIES_COMMAND = "-b";
+	private static final String SERIES_COMMAND = "-s";
+	private static final Map<String, List<String>> COMMANDS = Map.ofEntries(
+		new AbstractMap.SimpleEntry<String, List<String>>(BABIES_COMMAND, new ArrayList<>(List.of(".jpg"))),
+		new AbstractMap.SimpleEntry<String, List<String>>(SERIES_COMMAND, new ArrayList<>(List.of(".mp4")))
+	);
+
 	public static void main(String[] args) {
 		try {
+			String command = args[COMMAND_INDEX];
+			int extension = Integer.parseInt(args[EXTENSION_INDEX]);
+
 			System.out.println(" === Loading directory ===");
-			File dir = new File(args[1]);
-			List<String> names = new ArrayList<>();
+			File dir = new File(args[DIR_INDEX]);
 
 			System.out.println(" === Loading files ===");
 			File[] files = dir.listFiles(new FilenameFilter() {
 				public boolean accept(File dir, String name) {
-					return name.toLowerCase().endsWith(".mp4");
+					return name.toLowerCase().endsWith(COMMANDS.get(command).get(extension));
 				}
 			});
 			for (File f : files) {
@@ -26,18 +39,25 @@ public class RenameFilesUtility {
 			}
 
 			System.out.println(" === Sorting files ===");
-			Arrays.sort(files, Comparator.comparingLong(File::lastModified));
-			for (File f : files) {
-				System.out.println(f.getName());
-			}
+			// Arrays.sort(files, Comparator.comparingLong(File::lastModified));
+			// for (File f : files) {
+			// 	System.out.println(f.getName());
+			// }
 
-			System.out.println(" === New names loaded ===");
-			File name = new File(args[1] + "names.txt");
-			Scanner s = new Scanner(name);
-			while (s.hasNextLine()) {
-				names.add(s.nextLine() + ".mp4");
+			Scanner s = null;
+			List<String> names = new ArrayList<>();
+			if (command.equals(BABIES_COMMAND)) {
+			} else if (command.equals(SERIES_COMMAND)) {
+				System.out.println(" === New names loaded ===");
+				File name = new File(args[DIR_INDEX] + "names.txt");
+				s = new Scanner(name);
+				while (s.hasNextLine()) {
+					names.add(s.nextLine() + extension);
+				}
+				s.close();
+			} else {
+				throw new Exception(" === Invalid command used! ===");
 			}
-			s.close();
 
 			System.out.println(" === Do you want to continue with the renaming process? (y/n) ===");
 			s = new Scanner(System.in);
@@ -46,8 +66,8 @@ public class RenameFilesUtility {
 				System.out.println(" === Files renamed, step 2 ===");
 				if (files.length == names.size()) {
 					for (int i = 0; i < files.length; i++) {
-						files[i].renameTo(new File(args[1] + names.get(i)));
-						System.out.println("[" + files[i].getName() + "] renamed to: [" + names.get(i) + "]");
+						files[i].renameTo(new File(args[DIR_INDEX] + names.get(i)));
+						System.out.println(files[i].getName() + " was renamed to " + names.get(i));
 					}
 
 					System.out.println(" === Do you want to undo the renaming process? (y/n) ===");
@@ -56,26 +76,32 @@ public class RenameFilesUtility {
 						System.out.println(" === Reloading undo files ===");
 						File[] undo = dir.listFiles(new FilenameFilter() {
 							public boolean accept(File dir, String name) {
-								return name.toLowerCase().endsWith(".mp4");
+								return name.toLowerCase().endsWith(COMMANDS.get(command).get(extension));
 							}
 						});
-						Arrays.sort(undo, Comparator.comparingLong(File::lastModified));
-						for (File f : undo) {
-							System.out.println(f.getName());
-						}
-						System.out.println(" === Files renamed, undo step ===");
-						for (int i = 0; i < undo.length; i++) {
-							undo[i].renameTo(new File(args[1] + files[i].getName()));
-							System.out.println("[" + undo[i].getName() + "] renamed to: [" + files[i].getName() + "]");
+
+						if (undo.length == files.length) {
+							Arrays.sort(undo, Comparator.comparingLong(File::lastModified));
+							for (File f : undo) {
+								System.out.println(f.getName());
+							}
+
+							System.out.println(" === Files are being restored, undo step ===");
+							for (int i = 0; i < undo.length; i++) {
+								undo[i].renameTo(new File(args[DIR_INDEX] + files[i].getName()));
+								System.out.println("[" + undo[i].getName() + "] renamed to: [" + files[i].getName() + "]");
+							}
+						} else {
+							System.out.println(" === Utility failed to process files... The amount of reloaded undo files and original files do not match... ===");
 						}
 					} else {			
-						System.out.println("Files are not restored...");
+						System.out.println(" === You chose not to undo the last renaming process, aborting the undo process... ===");
 					}
 				} else {			
-					System.out.println("Utility failed to process files...");
+					System.out.println(" === Utility failed to process files... The amount of generated names and amount of files do not match... ===");
 				}
 			} else {			
-				System.out.println("Files are not renamed...");
+				System.out.println(" === You chose not to rename files, aborting the renaming process... ===");
 			}
 			s.close();
 		} catch (Exception e) {
