3a8f4155de41b4e43f886f930dd60b6cc3525c82
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-09-12 13:11:51 +0200

    [DataHandlingManager] Switch case refactoring
    We are now introducing this change to clean up and
    renew the code to follow the newer standards, as of
    Java 13 we now have Switch Expressions standardized.

--- src/main/java/org/hellstrand/renfi/manager/DataHandlingManager.java
+++ src/main/java/org/hellstrand/renfi/manager/DataHandlingManager.java
@@ -1,10 +1,9 @@
 package org.hellstrand.renfi.manager;
 
-import static org.hellstrand.renfi.constant.Constants.FAILURE;
 import static org.hellstrand.renfi.constant.Constants.IMAGE_PROCESSING;
 import static org.hellstrand.renfi.constant.Constants.JAVA_PROCESSING;
 import static org.hellstrand.renfi.constant.Constants.LIST_PROCESSING;
-import static org.hellstrand.renfi.constant.Constants.MESSAGE_INVALID_USE;
+import static org.hellstrand.renfi.constant.Constants.MESSAGE_EXECUTION_ABORT;
 import static org.hellstrand.renfi.constant.Constants.INPUT_SOURCE;
 import static org.hellstrand.renfi.constant.Constants.ORIGIN_PROCESSING;
 import static org.hellstrand.renfi.constant.Constants.VIDEO_PROCESSING;
@@ -18,7 +17,7 @@ import org.hellstrand.renfi.util.VideoProcessingUtil;
 
 /**
  * @author (Mats Richard Hellstrand)
- * @version (6th of September, 2023)
+ * @version (12th of September, 2025)
  */
 public class DataHandlingManager {
     public static void processBranch(
@@ -26,26 +25,24 @@ public class DataHandlingManager {
         String inputSourceName = path.concat(INPUT_SOURCE);
 
         switch (branch) {
-            case JAVA_PROCESSING: // Prepare conversion history based on Java +7...
+            case JAVA_PROCESSING -> { // Prepare conversion history based on Java +7...
                 NioProcessingUtil.prepareHistoryByNioProcessing(files, history, fromExtension, dateTypeFlag);
-                break;
-            case ORIGIN_PROCESSING: // Prepare conversion history based on origin data with Drew Noakes's extractor...
+            }
+            case ORIGIN_PROCESSING -> { // Prepare conversion history based on origin data with Drew Noakes's extractor...
                 if (resourceType.equals(VIDEO_PROCESSING)) {
                     VideoProcessingUtil.prepareHistoryByOrigin(files, history, fromExtension);
                 } else if (resourceType.equals(IMAGE_PROCESSING)) {
                     ImageProcessingUtil.prepareHistoryByOrigin(files, history, fromExtension);
                 }
-                break;
-            case LIST_PROCESSING: // Prepare conversion history based on file input...
+            }
+            case LIST_PROCESSING -> { // Prepare conversion history based on file input...
                 if (resourceType.equals(VIDEO_PROCESSING)) {
                     VideoProcessingUtil.prepareHistoryByInput(files, history, inputSourceName, fromExtension);
                 } else if (resourceType.equals(IMAGE_PROCESSING)) {
                     ImageProcessingUtil.prepareHistoryByInput(files, history, inputSourceName, fromExtension);
                 }
-                break;
-            default:
-                printMessage(MESSAGE_INVALID_USE);
-                System.exit(FAILURE);
+            }
+            default -> printMessage(MESSAGE_EXECUTION_ABORT);
         }
     }
 }
