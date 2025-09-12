1f7b12c8fa44a9780356190af5937e53051b6a6d
Mats Richard Hellstrand <goldenmaza@outlook.com>
2025-09-12 12:14:40 +0200

    [HelpGuideUtil] Text block refactoring
    The HelpGuideUtil class has gotten a minor
    refactoring to use the Text block feature,
    instead of StringBuilder, to present the
    guide in a printout.

--- src/main/java/org/hellstrand/renfi/util/HelpGuideUtil.java
+++ src/main/java/org/hellstrand/renfi/util/HelpGuideUtil.java
@@ -26,62 +26,82 @@ import java.util.List;
 
 /**
  * @author (Mats Richard Hellstrand)
- * @version (5th of September, 2023)
+ * @version (12th of September, 2025)
  */
-public class HelpGuideUtil {
+public final class HelpGuideUtil {
+    private HelpGuideUtil() {}
+
     public static void displayHelpGuide() {
-        StringBuilder sb = new StringBuilder();
         List<String> imageExtensions = PROCESSING_SUPPORT.get(IMAGE_PROCESSING);
         List<String> videoExtensions = PROCESSING_SUPPORT.get(VIDEO_PROCESSING);
         String helpFlags = HELP_FLAGS.toString()
             .replace("[", "(")
             .replace("]", ")");
 
-        sb.append("  === Help Guide ===")
-            .append("\n")
-            .append("\n  Operation legend:")
-            .append("\n")
-            .append("\n  java -jar Renfi.jar <FLOW> <BRANCH> <PATH> <RESOURCE_TYPE> <FROM_EXTENSION> <TO_EXTENSION> <X_AXIS> <Y_AXIS> <DATE_TYPE>")
-            .append("\n")
-            .append("\n  (FLOW)\t\t\t\tThe desired flow of the application: ").append(FLOW_FLAGS)
-            .append("\n  (BRANCH)\t\t\t\tThe desired branch of the application: ").append(BRANCH_FLAGS)
-            .append("\n  (PATH)\t\t\t\tThe directory path to where the files are located, e.g. \"c:/directory/\" OR \"c/directory/\"")
-            .append("\n  (RESOURCE_TYPE)\t\tTo focus on either resource type: ").append(RESOURCE_FLAGS)
-            .append("\n  (FROM_EXTENSION)\t\tThe desired file extension based on index (INTEGER). (view 'Extension legend' below)")
-            .append("\n  (TO_EXTENSION)\t\tThe desired file extension based on index (INTEGER). (view 'Extension legend' below)")
-            .append("\n  (X_AXIS)\t\t\t\tThe starting pixel on the X axis (INTEGER).")
-            .append("\n  (Y_AXIS)\t\t\t\tThe starting pixel on the Y axis (INTEGER).")
-            .append("\n  (DATE_TYPE)\t\t\tThe desired date type flag from file: ").append(TYPE_FLAGS)
-            .append("\n")
-            .append("\n  Extension legend:")
-            .append("\n  (IMAGE)\t\t\t\tThe supported extensions and their indexes: ").append(printValues(imageExtensions))
-            .append("\n  (VIDEO)\t\t\t\tThe supported extensions and their indexes *: ").append(printValues(videoExtensions))
-            .append("\n  \t\t\t\t\t\t* Not ALL video formats are supported by Drew Noakes's extractor, some might need Java +7 to fetch date...")
-            .append("\n")
-            .append("\n  Flag legend:")
-            .append("\n  (" + IMAGE_PROCESSING + ")\t\t\t\t\tTo process IMAGE files based on extension stated.")
-            .append("\n  (" + VIDEO_PROCESSING + ")\t\t\t\t\tTo process VIDEO files based on extension stated.")
-            .append("\n")
-            .append("\n  (" + FILE_PROCESSING + ")\t\t\t\t\tThe File processing flow lets us manipulate the file itself or to analyze it. You can choose one of the following branches:")
-            .append("\n  (" + COMPARE_PROCESSING + ")\t\t\t\t\tTo compare files, pixel by pixel, to determine if they are a duplicate or a close match.")
-            .append("\n  (" + CROP_PROCESSING + ")\t\t\t\tTo crop images, based on X & Y coordinates, this will remove the borders on both sides of the image.")
-            .append("\n  (" + CONVERT_PROCESSING + ")\t\t\t\tTo convert images from one extension to another.")
-            .append("\n  (" + DETECT_PROCESSING + ")\t\t\t\tTo detect black borders and to sort them into folders based on detected height. Note: Currently, only top-down is supported!")
-            .append("\n  (" + SOURCE_PROCESSING + ")\t\t\t\t\tTo prepare a SOURCE FILE based on directory files.")
-            .append("\n")
-            .append("\n  (" + DATA_PROCESSING + ")\t\t\t\t\tThe Data processing flow lets us create a source file or determine date and time. You can choose one of the following branches:")
-            .append("\n  (" + JAVA_PROCESSING + ")\t\t\t\t\tTo use Java +7 to determine the date of creation.")
-            .append("\n  (" + ORIGIN_PROCESSING + ")\t\t\t\t\tTo prepare history conversion based on ORIGIN DATA by using Drew Noakes's extractor.")
-            .append("\n  (" + LIST_PROCESSING + ")\t\t\t\t\tTo prepare history conversion based on SOURCE FILE.")
-            .append("\n")
-            .append("\n  (" + CREATION_TIME_FLAG + ")\t\t\t\t\tTo use the Creation Time field for setting the date and time.")
-            .append("\n  (" + LAST_MODIFIED_TIME_FLAG + ")\t\t\t\tTo use the Last Modified Time field for setting the date and time.")
-            .append("\n  (" + LAST_ACCESS_TIME_FLAG + ")\t\t\t\tTo use the Last Access Time field for setting the date and time.")
-            .append("\n")
-            .append("\n  ")
-            .append(helpFlags).append("\t\tTo display this help guide.");
+        String guideDisplayed = """
+            === Help Guide ===
 
-        System.out.println(sb);
+            \tOperational overview:
+
+            \tjava -jar Renfi.jar <FLOW> <BRANCH> <PATH> <RESOURCE_TYPE> <FROM_EXTENSION> <TO_EXTENSION> <X_AXIS> <Y_AXIS> <DATE_TYPE>
+
+            \t(FLOW)\t\t\t\tThe desired flow of the application:\040""" + FLOW_FLAGS + """
+            \n\t(BRANCH)\t\t\tThe desired branch of the application:\040""" + BRANCH_FLAGS + """
+            \n\t(PATH)\t\t\t\tThe directory path to where the files are located, e.g. "c:/directory/" OR "c/directory/"
+            \t(RESOURCE_TYPE)\t\tTo focus on either resource type:\040""" + RESOURCE_FLAGS + """
+            \n\t(FROM_EXTENSION)\tThe desired file extension based on index (INTEGER). (view 'Extension legend' below)
+            \t(TO_EXTENSION)\t\tThe desired file extension based on index (INTEGER). (view 'Extension legend' below)
+            \t(X_AXIS)\t\t\tThe starting pixel on the X axis (INTEGER).
+            \t(Y_AXIS)\t\t\tThe starting pixel on the Y axis (INTEGER).
+            \t(DATE_TYPE)\t\t\tThe desired date type flag from file:\040""" + TYPE_FLAGS + """
+
+
+            \n\tExtension legend:
+
+            \t(IMAGE)\t\t\t\tThe supported extensions and their indexes:\040""" + printValues(imageExtensions) + """
+            \n\t(VIDEO)\t\t\t\tThe supported extensions and their indexes *:\040""" + printValues(videoExtensions) + """
+            \n\t\t\t\t\t\t* Not ALL video formats are supported by Drew Noakes's extractor, some might need Java +7 (nio) to fetch date...
+
+            \tFlag legend:
+
+            \t(""" + IMAGE_PROCESSING + """
+            )\t\t\t\tTo process IMAGE files based on extension stated.
+            \t(""" + VIDEO_PROCESSING + """
+            )\t\t\t\tTo process VIDEO files based on extension stated.
+
+            \t(""" + FILE_PROCESSING + """
+            )\t\t\t\tThe File processing flow lets us manipulate the file itself or to analyze it. You can choose one of the following branches:
+            \t(""" + COMPARE_PROCESSING + """
+            )\t\t\t\tTo compare files, pixel by pixel, to determine if they are a duplicate or a close match.
+            \t(""" + CROP_PROCESSING + """
+            )\t\t\t\tTo crop images, based on X & Y coordinates, this will remove the borders on both sides of the image.
+            \t(""" + CONVERT_PROCESSING + """
+            )\t\t\t\tTo convert images from one extension to another.
+            \t(""" + DETECT_PROCESSING + """
+            )\t\t\t\tTo detect black borders and to sort them into folders based on detected height. Note: Currently, only top-down is supported!
+            \t(""" + SOURCE_PROCESSING + """
+            )\t\t\t\tTo prepare a SOURCE FILE based on directory files.
+
+            \t(""" + DATA_PROCESSING + """
+            )\t\t\t\tThe Data processing flow lets us create a source file or determine date and time. You can choose one of the following branches:
+            \t(""" + JAVA_PROCESSING + """
+            )\t\t\t\tTo use Java +7 (nio) to determine the date of creation.
+            \t(""" + ORIGIN_PROCESSING + """
+            )\t\t\t\tTo prepare history conversion based on ORIGIN DATA by using Drew Noakes's extractor.
+            \t(""" + LIST_PROCESSING + """
+            )\t\t\t\tTo prepare history conversion based on SOURCE FILE.
+
+            \t(""" + CREATION_TIME_FLAG + """
+            )\t\t\t\tTo use the Creation Time field for setting the date and time.
+            \t(""" + LAST_MODIFIED_TIME_FLAG + """
+            )\t\t\t\tTo use the Last Modified Time field for setting the date and time.
+            \t(""" + LAST_ACCESS_TIME_FLAG + """
+            )\t\t\t\tTo use the Last Access Time field for setting the date and time.
+
+            \t""" + helpFlags + """
+            \tTo display this help guide.""";
+
+        System.out.println(guideDisplayed);
     }
 
     private static String printValues(List<String> strings) {
@@ -104,6 +124,4 @@ public class HelpGuideUtil {
 
         System.out.println(sb);
     }
-
-    private HelpGuideUtil() {}
 }
